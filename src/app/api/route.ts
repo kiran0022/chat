a5e473753557dd93224b130365873f83a795f945
Kiran Kumar K <kirankumarwaste1@gmail.com>
2024-02-03 00:58:14 +0530

    feat(webhook): add webhook discord

---
+++ src/app/api/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+export async function GET(req: NextRequest, res: NextResponse) {
+
+    return NextResponse.json("asda")
+}
+export async function POST(req: NextRequest, res: NextResponse) {
+    const data = await req.json()
+    console.log(data)
+    return NextResponse.json(data)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,22 @@ import SupabaseServerClient from "@/lib/supabase/server";
 export default async function Home() {
   const supabase = await SupabaseServerClient();
   const { data } = await supabase.auth.getSession();
+  console.log(data);
+
+  //wb change
+  // t
+  if (data.session) {
+    await fetch(process.env.DISCORD_WEBHOOK!, {
+      method: "POST",
+      body: JSON.stringify({
+        content: `${data.session?.user.email} just logged in`,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+  //wb change
 
   return (
     <>
