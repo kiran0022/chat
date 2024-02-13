54f7804dd1e087ba11c394b9d335f89e60a947af
Kiran Kumar K <kirankumarwaste1@gmail.com>
2024-02-14 00:56:10 +0530

    fix: loading

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest, res: NextResponse) {
-
     return NextResponse.json("asda")
 }
 export async function POST(req: NextRequest, res: NextResponse) {
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,44 +1,25 @@
 import React from "react";
 
 export default function loading() {
+  const arr = Array(5).fill(0);
+
   return (
     <div>
-      <div className="flex animate-pulse">
-        <div className="flex-shrink-0">
-          <span className="w-16 h-16 block bg-gray-200 rounded-full"></span>
-        </div>
-        <div className="ms-4 mt-2 w-full">
-          <h3 className="w-48 h-4 bg-gray-200 rounded-full"></h3>
-          <ul className="mt-5 space-y-3">
-            <li className="w-full h-4 bg-gray-200 rounded-full"></li>
-            <li className="w-full h-4 bg-gray-200 rounded-full"></li>
-            <li className="w-full h-4 bg-gray-200 rounded-full"></li>
-          </ul>
-        </div>
-      </div>
+      {arr.map((data) => (
+        <div key={data} className="">
+          <div className="flex">
+            <div className="relative isolate h-10 w-10 overflow-hidden rounded-full shadow-xl shadow-black/5 before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:border-t before:border-rose-100/10 before:bg-gradient-to-r before:from-transparent before:via-rose-100/10 before:to-transparent"></div>
 
-      {/* 
-
-
-      <div class="flex h-screen w-screen items-center justify-center bg-slate-600">
-        <div class="h-64 w-96 border">
-          <div class="flex">
-            <div class="h-10 w-10 rounded-full bg-white"></div>
-            <div class="bg-gradient-to-r from-transparent -translate-x-full animate-[shimmer_2s_infinite] via-violet-300 to-transparent">
-              {" "}
-            </div>
-            <div class="ms-4 flex flex-col space-y-2">
-              <div class="space-y-1">
-                <div class="h-3 w-36 rounded-md bg-slate-200"></div>
-                <div class="h-2 w-20 rounded-md bg-slate-200"></div>
+            <div className="ms-4 flex flex-col space-y-2">
+              <div className="space-y-1">
+                <div className="relative isolate h-3 w-36 overflow-hidden rounded-md shadow-xl shadow-black/5 before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:border-t before:border-rose-100/10 before:bg-gradient-to-r before:from-transparent before:via-rose-100/10 before:to-transparent"></div>
+                <div className="relative isolate h-2 w-20 overflow-hidden rounded-md shadow-xl shadow-black/5 before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:border-t before:border-rose-100/10 before:bg-gradient-to-r before:from-transparent before:via-rose-100/10 before:to-transparent"></div>
               </div>
-              <div class="h-7 w-64 rounded-md bg-slate-200"></div>
+              <div className="relative isolate h-7 w-64 overflow-hidden rounded-md shadow-xl shadow-black/5 before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:border-t before:border-rose-100/10 before:bg-gradient-to-r before:from-transparent before:via-rose-100/10 before:to-transparent"></div>
             </div>
           </div>
         </div>
-      </div> */}
-
-      {/* ------ */}
+      ))}
     </div>
   );
 }
